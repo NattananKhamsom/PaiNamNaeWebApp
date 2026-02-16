87be16eebfb2610ee562d8bcc57e8bdc9b491e34
NattananKhamsom <nattanan.kh@kkumail.com>
2026-02-17 01:47:48 +0700

    push

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,7 +1,13 @@
 export default defineNuxtConfig({
+  ssr: false, // ปิด SSR ให้เป็น SPA
+
   runtimeConfig: {
     public: {
       apiBase: 'https://myapp-backend-af5a.onrender.com/api'
     }
+  },
+
+  nitro: {
+    preset: 'static' // บอกให้ generate เป็น static
   }
 })
