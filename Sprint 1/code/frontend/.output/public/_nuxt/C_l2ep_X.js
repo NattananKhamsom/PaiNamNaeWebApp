03d811f95ebe75a538a0ad55a602604ccef8e8ba
Thanawin Polcho <thanawin.p@kkumail.com>
2026-03-04 23:04:28 +0700

    Delete Sprint 1/code/frontend directory

--- Sprint 1/code/frontend/.output/public/_nuxt/C_l2ep_X.js
+++
@@ -1,1 +0,0 @@
-import{a7 as o,J as t,C as a}from"./CfIHfp8L.js";const s=o(e=>{if(!t("token").value&&e.path!=="/login")return a("/login")});export{s as default};
--- Sprint 1/code/frontend/.output/public/_nuxt/DM2dqQF2.js
+++
@@ -1,1 +0,0 @@
-import{a7 as a,J as o,C as t}from"./CfIHfp8L.js";const i=a((n,r)=>{const e=o("user").value;if(!o("token").value)return t("/login");if(!e||e.role!=="ADMIN")return t("/")});export{i as default};
